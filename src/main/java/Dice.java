b1671fdc67c435cadd1d5b3c2f3efc9fbed8d0a2
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-02-19 16:48:15 -0500

    first commit

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,19 @@
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+    public Integer numDice;
+    public Random r = new Random();
+    public Dice(Integer numDice){
+        this.numDice = numDice;
+    }
+
+    public Integer tossAndSum(){
+        Random random = new Random();
+        Integer sum = 0;
+        for(int i = 0; i < numDice; i++){
+            sum+= random.nextInt(6)+1;
+        }
+        return sum;
+    }
 
 }
