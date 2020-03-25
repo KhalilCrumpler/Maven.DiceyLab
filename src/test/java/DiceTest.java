16b876c855220dec94e95a9a312847e9aeb5d260
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-25 06:25:29 -0400

    added test

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -4,6 +4,20 @@ import org.junit.Test;
 public class DiceTest {
 
     @Test
-    public void tossAndSum() {
+    public void testNumberOfDice() {
+        Simulation roll = new Simulation(2, 12);
+        Integer expected = 2;
+        roll.setDice(2);
+        Integer actual = roll.getDiceNumber();
+        Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void tossAndSumTest(){
+        Dice dice = new Dice(2);
+        Integer actual = dice.tossAndSum();
+        boolean expected = (actual > 0 && actual < 7);
+
+    }
+
 }
